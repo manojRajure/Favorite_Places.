6723f170a8e34ffc3248cfec4f8c18b4e12a8966
Manoj Rajure <rajuremanoj681@gmail.com>
2023-08-20 11:24:11 +0530

    commit

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,12 +2,20 @@ import 'dart:io';
 
 import 'package:favorite_place/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super([]);
 
-  void addPlaces(String title, File image) {
-    final newPlace = Place(title: title, image: image);
+  void addPlaces(String title, File image) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
+    final newPlace = Place(title: title, image: copiedImage);
     state = [newPlace, ...state];
   }
 }
