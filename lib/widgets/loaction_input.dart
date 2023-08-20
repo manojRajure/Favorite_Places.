bff2027c4709fd6633c02f710121f6d816efb462
Manoj Rajure <rajuremanoj681@gmail.com>
2023-08-20 09:08:42 +0530

    commit

--- lib/widgets/loaction_input.dart
+++ lib/widgets/loaction_input.dart
@@ -9,7 +9,6 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _pickedLocation;
   var _isGettingLocation = false;
 
   void _getCurrentLoaction() async {
